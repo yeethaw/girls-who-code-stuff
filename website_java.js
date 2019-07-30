2b522c981410eb96bff64623859158f85dbc921a
Veronika <41167450+yeethaw@users.noreply.github.com>
2019-07-30 15:47:03 -0400

    updatin website and images

---
+++ website_java.js
@@ -0,0 +1,9 @@
+
+function imgshow() {
+  var x = document.getElementById("skyfall");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
