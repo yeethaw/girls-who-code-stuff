97f3001825b92eaa6303a80c9e209a41b79e6f65
Veronika <41167450+yeethaw@users.noreply.github.com>
2019-08-08 15:51:15 -0400

    Add files via upload

---
+++ maze_runner.ino
@@ -0,0 +1,82 @@
+#include <Servo.h>                           // Include servo library
+
+int left_whisker = 5;
+int right_whisker = 7;
+
+Servo servoRight;
+Servo servoLeft;
+
+
+void forwards(int delayy){
+  servoLeft.writeMicroseconds(1700);        
+  servoRight.writeMicroseconds(1300);
+  delay(delayy);
+}
+
+void backwards(int delayy){
+  servoLeft.writeMicroseconds(1300);        
+  servoRight.writeMicroseconds(1700);
+  delay(delayy);
+}
+
+void spin_left(int delayy){
+  servoLeft.writeMicroseconds(1300);        
+  servoRight.writeMicroseconds(1300);
+  delay(delayy);
+}
+
+void spin_right(int delayy){
+  servoLeft.writeMicroseconds(1700);        
+  servoRight.writeMicroseconds(1700);
+  delay(delayy);
+}
+
+void moveKitty() {
+  // Your Code Here
+}
+
+void stopKitty(int delayy) {
+  // Your Code Here
+  servoLeft.writeMicroseconds(1500);        
+  servoRight.writeMicroseconds(1500);
+  delay(delayy);
+}
+
+void setup() {
+  servoLeft.attach(13);                      
+  servoRight.attach(12); 
+
+  pinMode(left_whisker , INPUT);
+  pinMode(right_whisker, INPUT);
+  
+  Serial.begin(9600);          //use the serial port
+  Serial.println("setup completed!");
+ 
+}
+
+void loop(){
+  // put your main code here, to run repeatedly:
+
+  int left_whisker_value = digitalRead(left_whisker);
+  int right_whisker_value = digitalRead(right_whisker); 
+
+  if (left_whisker_value == 1 && right_whisker_value == 1){
+    Serial.println("ONWARDS TO BERLIN!!");
+    forwards(500);
+  }
+
+  if (left_whisker_value == 0 && right_whisker_value == 0) {
+    Serial.println("RETREAT");
+    backwards(2000);
+    spin_left(700);
+  }
+
+  if (left_whisker_value == 0 && right_whisker_value == 1){
+    Serial.println("SPINNING RIGHT");   
+    spin_right(500);
+  }
+  if (left_whisker_value == 1 && right_whisker_value == 0){
+    Serial.println("SPINNING LEFT");   
+    spin_left(500);
+  }
+}
